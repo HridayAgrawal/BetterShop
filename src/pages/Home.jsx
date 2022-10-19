44cf0544ba18babf2b4e10c65c30da778770695f
Hriday Agrawal <hriday.agrawal03@gmail.com>
2022-10-19 22:35:53 +0530

    navbar

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Announcement from "../Components/Announcement";
+import Navabr from "../Components/Navabr";
+import Slider from "../Components/Slider";
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <Navabr />
+      <Slider />
+    </div>
+  );
+};
+
+export default Home;
