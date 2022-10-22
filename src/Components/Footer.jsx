b472571b093a84dade999ce25e309aa8fe0058c4
Hriday Agrawal <hriday.agrawal03@gmail.com>
2022-10-22 23:26:14 +0530

    footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,12 +4,24 @@ import styled from "styled-components";
 const Container = styled.div`
   display: flex;
 `;
-const Left = styled.div``;
-const Center = styled.div``;
-const Right = styled.div``;
+const Left = styled.div`
+  flex: 1;
+`;
+const Center = styled.div`
+  flex: 1;
+`;
+const Right = styled.div`
+  flex: 1;
+`;
 
 const Footer = () => {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Left></Left>
+      <Center></Center>
+      <Right></Right>
+    </Container>
+  );
 };
 
 export default Footer;
