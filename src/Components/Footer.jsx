32d326dcfbd3810e2ec0fb90d5f490e1afde2cc1
Hriday Agrawal <hriday.agrawal03@gmail.com>
2022-10-24 13:40:38 +0530

    Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,3 +1,4 @@
+import { Facebook, Instagram, Pinterest, Twitter } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
 
@@ -6,7 +7,30 @@ const Container = styled.div`
 `;
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
 `;
+
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+const Socialcontainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+
 const Center = styled.div`
   flex: 1;
 `;
@@ -19,7 +43,25 @@ const Footer = () => {
     <Container>
       <Left>
         <Logo>BetterShop.</Logo>
-        <Desc></Desc>
+        <Desc>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
+          nisi magni, quibusdam voluptatibus asperiores saepe cum aut voluptate
+          a exercitationem, sit numquam, explicabo ipsa ut quos mollitia ad.
+        </Desc>
+        <Socialcontainer>
+          <SocialIcon color="3B5999">
+            <Facebook />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <Instagram />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <Twitter />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <Pinterest />
+          </SocialIcon>
+        </Socialcontainer>
       </Left>
       <Center></Center>
       <Right></Right>
