b6134bceff6883517214593bbc43be81d750acdf
Hriday Agrawal <hriday.agrawal03@gmail.com>
2022-11-14 18:16:21 +0530

    commit

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -6,37 +6,46 @@ import { useLocation } from "react-router-dom";
 import styled from "styled-components";
 import Announcement from "../Components/Announcement";
 import Footer from "../Components/Footer";
-import Navabr from "../Components/Navbar";
+import Navbar from "../Components/Navbar";
 import Newsletter from "../Components/Newsletter";
 import { publicRequest } from "../requestMethods";
 import { mobile } from "../responsive";
+import { useDispatch } from "react-redux";
+import { addProduct } from "../redux/cartRedux";
 
 const Container = styled.div``;
+
 const Wrapper = styled.div`
   padding: 50px;
   display: flex;
   ${mobile({ padding: "10px", flexDirection: "column" })}
 `;
+
 const ImgContainer = styled.div`
   flex: 1;
 `;
-const InfoContainer = styled.div`
-  flex: 1;
-  padding: 0px 50px;
-  ${mobile({ padding: "10px" })}
-`;
+
 const Image = styled.img`
   width: 100%;
   height: 90vh;
   object-fit: cover;
   ${mobile({ height: "40vh" })}
 `;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+  ${mobile({ padding: "10px" })}
+`;
+
 const Title = styled.h1`
   font-weight: 200;
 `;
+
 const Desc = styled.p`
   margin: 20px 0px;
 `;
+
 const Price = styled.span`
   font-weight: 100;
   font-size: 40px;
@@ -49,10 +58,17 @@ const FilterContainer = styled.div`
   justify-content: space-between;
   ${mobile({ width: "100%" })}
 `;
+
 const Filter = styled.div`
   display: flex;
   align-items: center;
 `;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
 const FilterColor = styled.div`
   width: 20px;
   height: 20px;
@@ -61,10 +77,7 @@ const FilterColor = styled.div`
   margin: 0px 5px;
   cursor: pointer;
 `;
-const FilterTitle = styled.span`
-  font-size: 20px;
-  font-weight: 200;
-`;
+
 const FilterSize = styled.select`
   margin-left: 10px;
   padding: 5px;
@@ -85,6 +98,7 @@ const AmountContainer = styled.div`
   align-items: center;
   font-weight: 700;
 `;
+
 const Amount = styled.span`
   width: 30px;
   height: 30px;
@@ -95,7 +109,8 @@ const Amount = styled.span`
   justify-content: center;
   margin: 0px 5px;
 `;
-const Button1 = styled.button`
+
+const Button = styled.button`
   padding: 15px;
   border: 2px solid teal;
   background-color: white;
@@ -114,13 +129,14 @@ const Product = () => {
   const [quantity, setQuantity] = useState(1);
   const [color, setColor] = useState("");
   const [size, setSize] = useState("");
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const getProduct = async () => {
       try {
         const res = await publicRequest.get("/products/find/" + id);
         setProduct(res.data);
-      } catch (error) {}
+      } catch {}
     };
     getProduct();
   }, [id]);
@@ -134,12 +150,11 @@ const Product = () => {
   };
 
   const handleClick = () => {
-    //Update Cart
+    dispatch(addProduct({ ...product, quantity, color, size }));
   };
-
   return (
     <Container>
-      <Navabr />
+      <Navbar />
       <Announcement />
       <Wrapper>
         <ImgContainer>
@@ -148,7 +163,7 @@ const Product = () => {
         <InfoContainer>
           <Title>{product.title}</Title>
           <Desc>{product.desc}</Desc>
-          <Price>{product.price}</Price>
+          <Price>$ {product.price}</Price>
           <FilterContainer>
             <Filter>
               <FilterTitle>Color</FilterTitle>
@@ -171,7 +186,7 @@ const Product = () => {
               <Amount>{quantity}</Amount>
               <Add onClick={() => handleQuantity("inc")} />
             </AmountContainer>
-            <Button1 onClick={handleClick}>ADD TO CART</Button1>
+            <Button onClick={handleClick}>ADD TO CART</Button>
           </AddContainer>
         </InfoContainer>
       </Wrapper>
---
+++ client/src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartreducer from "./cartRedux";
+
+export default configureStore({
+  reducer: {
+    cart: cartreducer,
+  },
+});
