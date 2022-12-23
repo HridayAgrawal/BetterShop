ce917aa2303892e91716ddd71357de879b465a09
Hriday Agrawal <hriday.agrawal03@gmail.com>
2022-12-23 22:25:03 +0530

    commit

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzNjkxMjAyYjFkYjM3ODI1NjMzNjNhOCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2ODM0NDE4MSwiZXhwIjoxNjY4NjAzMzgxfQ.C5lyplodJKfS-Vgp8QkWVqViB8jYBT5t1OpUnSVeySg";
+
+const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
+const currentUser = user && JSON.parse(user).currentUser;
+const TOKEN = currentUser?.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
